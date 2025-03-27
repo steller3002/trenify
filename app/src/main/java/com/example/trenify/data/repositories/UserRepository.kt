d5a06ab3d40b359956c3062a11e5fd1fb0e62e74
stellerrs <bellerstas3002@gmail.com>
2025-03-28 00:55:35 +0400

    Fix bags

--- app/src/main/java/com/example/trenify/data/repositories/UserRepository.kt
+++ app/src/main/java/com/example/trenify/data/repositories/UserRepository.kt
@@ -1,15 +1,8 @@
 package com.example.trenify.data.repositories
 
 import com.example.trenify.data.dao.UserDao
-import com.example.trenify.data.entities.UserEntity
-import com.example.trenify.domain.models.User
-import kotlinx.coroutines.flow.Flow
-import javax.inject.Inject
 
-class UserRepository @Inject constructor(
+class UserRepository(
     private val userDao: UserDao
 ) {
-    suspend fun getAllUsers() : Flow<List<User>> {
-
-    }
 }
